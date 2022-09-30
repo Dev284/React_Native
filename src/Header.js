f5efa60c377b899da45a869d286318f0c0aaff9a
Dev284 <dev.kumar.hestabit@gmail.com>
2022-09-30 20:17:56 +0530

    Todo ADD

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Text>Header</Text>
+      <Text>TODO App</Text>
       <StatusBar style="auto" />
     </View>
   );
